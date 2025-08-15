2bd8d5ba62b77932cb792602f374377b517dc0a9
Saif Ahmed <saif_ahmed06@hotmail.com>
2025-08-15 21:38:26 +0100

    add comments

--- CarInfoSearch.py
+++ CarInfoSearch.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+#Get the brand name the user inputs to go to the wikipedia page of the car brand
 def get_brand_info(brand_name):
     url = f'https://en.wikipedia.org/wiki/{brand_name}'
+#Get the html text of the page
     html_text = requests.get(url).get_text()
+#Create instance of BeautifulSoup, parse through the html with lxml
     soup = BeautifulSoup(html_text, 'lxml')
+#Find every "tr" in the page's html
     table_row = soup.find_all('tr')
 
     new_brand_name = brand_name.replace("_", " ").replace("-", " ")
@@ -17,4 +21,3 @@ def get_brand_info(brand_name):
         if table_header and "Founders" in table_header:
             founder_name = table_header.find('td')
             print(f"{new_brand_name.title()} was founded by {founder_name}")
-            
