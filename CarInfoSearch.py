5200409168742d99f9e37e814d8fe5261d19b48e
Saif Ahmed <saif_ahmed06@hotmail.com>
2025-08-15 22:13:31 +0100

    add .split() within every if statement in for loop

--- CarInfoSearch.py
+++ CarInfoSearch.py
@@ -20,19 +20,19 @@ def get_brand_info(brand_name):
         #find "th" and "Founded" in html
         if table_header and "Founded" in table_header:
             #find the data inside the html
-            date_found = table_header.find('td').text
+            date_found = table_header.find('td').text.split("(")[0].split("[")[0]
             car_info.append(f"{new_brand_name} was founded: {date_found}")
         if table_header and "Founders" in table_header:
-            founder_names = table_header.find('td').text
+            founder_names = table_header.find('td').text.split("(")[0].split("[")[0]
             car_info.append(f"{new_brand_name.title()} was founded by {founder_names}")
         if table_header and "Founder" in table_header:
-            founder_name = table_header.find('td').text
+            founder_name = table_header.find('td').text.split("(")[0].split("[")[0]
             car_info.append(f"The founder of {new_brand_name} is: {founder_name}")
         if table_header and "Headquarters" in table_header:
-            location = table_header.find("td").text
+            location = table_header.find("td").text.split("(")[0].split("[")[0]
             car_info.append("The headquarters of {new_brand_name} are located in: {location}")
         if table_header and "Revenue" in table_header:
-            total_money = table_header.find("td").text
+            total_money = table_header.find("td").text.split("[")[0]
             car_info.append(f"The revenue for {new_brand_name} is: {total_money}")
 
     #This ensure the bot responds to the user if no information is found
