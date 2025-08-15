267a59694cf8bd299beb1f762700fbfbf769933d
Saif Ahmed <saif_ahmed06@hotmail.com>
2025-08-15 22:43:45 +0100

    change table_header.find to row.find

--- CarInfoSearch.py
+++ CarInfoSearch.py
@@ -5,7 +5,7 @@ import requests
 def get_brand_info(brand_name):
     url = f'https://en.wikipedia.org/wiki/{brand_name}'
 #Get the html text of the page
-    html_text = requests.get(url).get_text()
+    html_text = requests.get(url).text
 #Create instance of BeautifulSoup, parse through the html with lxml
     soup = BeautifulSoup(html_text, 'lxml')
 #Find every "tr" in the page's html
@@ -20,19 +20,19 @@ def get_brand_info(brand_name):
         #find "th" and "Founded" in html
         if table_header and "Founded" in table_header:
             #find the data inside the html
-            date_found = table_header.find('td').text.split("(")[0].split("[")[0]
+            date_found = row.find('td').text.split("(")[0].split("[")[0]
             car_info.append(f"{new_brand_name} was founded: {date_found}")
         if table_header and "Founders" in table_header:
-            founder_names = table_header.find('td').text.split("(")[0].split("[")[0]
+            founder_names = row.find('td').text.split("(")[0].split("[")[0]
             car_info.append(f"{new_brand_name.title()} was founded by {founder_names}")
         if table_header and "Founder" in table_header:
-            founder_name = table_header.find('td').text.split("(")[0].split("[")[0]
+            founder_name = row.find('td').text.split("(")[0].split("[")[0]
             car_info.append(f"The founder of {new_brand_name} is: {founder_name}")
         if table_header and "Headquarters" in table_header:
-            location = table_header.find("td").text.split("(")[0].split("[")[0]
+            location = row.find("td").text.split("(")[0].split("[")[0]
             car_info.append(f"The headquarters of {new_brand_name} are located in: {location}")
         if table_header and "Revenue" in table_header:
-            total_money = table_header.find("td").text.split("[")[0]
+            total_money = row.find("td").text.split("[")[0]
             car_info.append(f"The revenue for {new_brand_name} is: {total_money}")
 
     #This ensure the bot responds to the user if no information is found
