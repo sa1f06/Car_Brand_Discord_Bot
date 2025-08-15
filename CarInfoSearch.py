45b56189809ef394167bec612b330d67e924cae0
Saif Ahmed <saif_ahmed06@hotmail.com>
2025-08-15 22:03:33 +0100

    remove print statements from if statements, replace with list append

--- CarInfoSearch.py
+++ CarInfoSearch.py
@@ -21,16 +21,16 @@ def get_brand_info(brand_name):
         if table_header and "Founded" in table_header:
             #find the data inside the html
             date_found = table_header.find('td').text
-            print(f"{new_brand_name} was founded: {date_found}")
+            car_info.append(f"{new_brand_name} was founded: {date_found}")
         if table_header and "Founders" in table_header:
             founder_names = table_header.find('td').text
-            print(f"{new_brand_name.title()} was founded by {founder_names}")
+            car_info.append(f"{new_brand_name.title()} was founded by {founder_names}")
         if table_header and "Founder" in table_header:
             founder_name = table_header.find('td').text
-            print(f"The founder of {new_brand_name} is: {founder_name}")
+            car_info.append(f"The founder of {new_brand_name} is: {founder_name}")
         if table_header and "Headquarters" in table_header:
             location = table_header.find("td").text
-            print("The headquarters of {new_brand_name} are located in: {location}")
+            car_info.append("The headquarters of {new_brand_name} are located in: {location}")
         if table_header and "Revenue" in table_header:
             total_money = table_header.find("td").text
-            print(f"The revenue for {new_brand_name} is: {total_money}")
+            car_info.append(f"The revenue for {new_brand_name} is: {total_money}")
