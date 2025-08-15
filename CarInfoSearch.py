be20bd46ebf33d6bf940d2c32fc443374c3d3906
Saif Ahmed <saif_ahmed06@hotmail.com>
2025-08-15 21:21:18 +0100

    create car variable to find every tr in wikipedia's html

--- CarInfoSearch.py
+++ CarInfoSearch.py
@@ -5,4 +5,6 @@ def get_brand_info(brand_name):
     url = f'https://en.wikipedia.org/wiki/{brand_name}'
     html_text = requests.get(url).get_text()
     soup = BeautifulSoup(html_text, 'lxml')
+    car = soup.find_all('tr')
+
     
